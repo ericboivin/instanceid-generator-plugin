6e0ed25ffe021e3a903189f2a1e3e72496d6c44b
shanypalardy <shany.palardy@bell.ca>
2017-08-23 16:18:38 -0400

    Remove main test method

--- src/RandomGeneratorInstanceIdDocument.java
+++ src/RandomGeneratorInstanceIdDocument.java
@@ -97,22 +97,4 @@ public class RandomGeneratorInstanceIdDocument extends AnAction {
         }
         return sb.toString();
     }
-
-    public static void main(String[] args) {
-        // Quick test in a main to be sure the replacement is done corretly
-        updateInstanceIdWithUniqueId("<root available-locales=\"fr_CA,en_CA\" default-locale=\"fr_CA\">\n" +
-                "  <dynamic-element name=\"Title\" type=\"text\" index-type=\"keyword\" index=\"0\" instance-id=\"nqzd\">\n" +
-                "    <dynamic-content language-id=\"fr_CA\"><![CDATA[Conditions de l’offre]]></dynamic-content>\n" +
-                "    <dynamic-content language-id=\"en_CA\"><![CDATA[Offer details]]></dynamic-content>\n" +
-                "  </dynamic-element>\n" +
-                "  <dynamic-element name=\"Id\" type=\"text\" index-type=\"keyword\" index=\"0\" instance-id=\"laxi\">\n" +
-                "    <dynamic-element name=\"Legal_Note\" index=\"0\" instance-id=\"asvm\" type=\"text_box\" index-type=\"keyword\">\n" +
-                "      <dynamic-content language-id=\"fr_CA\"><![CDATA[Le prix promotionnel de 99,95 $/mois pour le forfait Bon est basé sur des crédits promotionnels appliqués au prix courant et est basé sur un abonnement continu à : Bell Télé Satellite forfait Bon  (43,95 $ moins un crédit mensuel de 10 $ pour 12 périodes de facturation complètes, plus les frais de service numérique de 3 $), et à la Téléphonie et Internet Haute vitesse de Télébec (76,01 $  moins le crédit promotionnel de 13,01 $ pour les mois 1 à 12). Des frais gouvernementaux de service 9-1-1 de 0,78 $/mois sont inclus.  Après la période promotionnelle, le tarif mensuel courant alors en vigueur s’appliquera. Les prix sont sujets à changements.]]></dynamic-content>\n" +
-                "      <dynamic-content language-id=\"en_CA\"><![CDATA[Promotional price of $99.95 for the Good bundle is based on promotional credits applies to the regular price based on continuous subscription to: Bell Satellite TV Good package ($43.95, plus $3 Digital Service Fee, less $10 credit for 12 full billing periods) and Télébec Home Phone and Internet services ($76.01, less $13.01 credit for months 1 to 12). A government 9-1-1 fee of $0.78/mo. is included.  After the promotional period, the then current price will apply.]]></dynamic-content>\n" +
-                "    </dynamic-element>\n" +
-                "    <dynamic-content language-id=\"fr_CA\"><![CDATA[1]]></dynamic-content>\n" +
-                "    <dynamic-content language-id=\"en_CA\"><![CDATA[1]]></dynamic-content>\n" +
-                "  </dynamic-element>\n" +
-                "</root>  ");
-    }
 }
